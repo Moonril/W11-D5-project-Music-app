8bc628b0d8fa826dd6ee620c5a5a138dfe95826f
Vincenza <vinni2690@hotmail.com>
2025-04-18 13:24:11 +0200

    fetch added

--- src/components/NuoveUscite.jsx
+++ src/components/NuoveUscite.jsx
@@ -1,21 +1,91 @@
-import { useState } from "react"
-import { Card, Col } from "react-bootstrap"
+import { useEffect, useState } from "react"
+import { Alert, Card, Col, Spinner } from "react-bootstrap"
+
+const API = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=electrcicallboys'
 
 const NuoveUscite = function () {
 
-    const [song, setSong] = useState()
+    const [songs, setSongs] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
+
+    const getSongs = () => {
+
+        fetch(API)
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error('response not ok')
+            }
+        })
+        .then((songsdata) => {
+            console.log('data', songsdata)
+            console.log('data', songsdata.data)
+            // oggetto -> array(25)
+            //img: data[0].album.cover
+            // song: data[0].album.title
+            // album: data[0].artist.name
+            // 0-2 + 4-10
+
+            /* const arrSongs = [...songsdata.data.slice(0, 2), ...songsdata.data.slice(4, 10)] */
+            setSongs([...songsdata.data.slice(0, 10)])
+            setIsLoading(false)
+        })
+        .catch((err) => {
+            console.log('Errore nella fetch:', err)
+
+            setIsLoading(false)
+            setIsError(true)
+        })
+
+    }
+
+    useEffect(()=>{
+        getSongs()
+    }, [])
 
 
     return(
-        <Col xs={4} md={2.4} >
-                    <Card className="border-0 bg-transparent">
-                        <Card.Img variant="top" src="2f.png" />
-                        <Card.Body className="px-0 bg-transparent text-light">
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>something</Card.Text>
-                        </Card.Body>
-                    </Card>
-        </Col>
+
+        <>
+        {/* spinner */}
+
+        {
+            isLoading === true && (
+                <div className="text-center">
+                    <Spinner variant="primary" animation="border"  />
+                </div>
+            )
+        }
+
+        {/* ERRORE */}
+            {
+                isError && (
+                    <Alert variant='danger' className="text-center">
+                        <i className="bi bi-bug"></i> Errore nella fetch <i className="bi bi-bug"></i>
+                    </Alert>
+                )
+            }
+
+        {
+            songs.map((song, i) =>(
+                
+                <Col xs={4} md={3} key={song.id} >
+                                <Card className="border-0 bg-transparent" >
+                                    <Card.Img variant="top" src={song.album.cover_big} />
+                                    <Card.Body className="px-0 bg-transparent text-light">
+                                        <Card.Title>{song.album.title}</Card.Title>
+                                        <Card.Text>{song.artist.name}</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                    </Col>
+                )
+                
+            )
+        }
+
+        </>
     )
 }
 
