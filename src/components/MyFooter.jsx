2c4956a17f5c53a25375729bb36d97b44834f203
Vincenza <vinni2690@hotmail.com>
2025-08-27 17:40:59 +0200

    footer updated

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,20 +1,22 @@
 import { Col, Row } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 const MyFooter = function () {
     return (
         <footer className="footer px-4 py-2 text-secondary">
             <Row className="text-start py-1">
-                <Col xs={2} className="text-light px-1">Italia</Col>
-                <Col xs={2}>English (uk)</Col>
+                <Col xs={4} md={3} lg={2} className="border-end border-secondary"><a href="" className="text-light text-decoration-none">Italia</a></Col>
+                <Col xs={4} md={3} lg={2} className="border-end border-secondary text-center"><a href="" className="text-secondary text-decoration-none">English (uk)</a></Col>
+                <Col xs={4} md={3} lg={2}><a href="" className="text-secondary text-decoration-none text-center">Español (España)</a></Col>
             </Row>
             <Row className="py-1">
-                <Col className="px-1">Copyright &copy; {new Date().getFullYear()} Apple Inc. Tutti i diritti riservati.</Col>
+                <Col>Copyright &copy; {new Date().getFullYear()} <span className="text-light">Apple Inc.</span> All rights reserved.</Col>
             </Row>
-            <Row className="py-1 text-start">
-                <Col xs={3} className=" px-1">condizioni dei servizi internet</Col>
-                <Col xs={2} className=" px-1">Apple Music e privacy</Col>
-                <Col xs={2} className=" px-1"> Avviso sui cookie</Col>
-                <Col xs={2} className=" px-1">Supporto</Col>
+            <Row className="py-1 text-center">
+                <Col xs={2} className="border-end border-secondary px-1"><a href="" className="text-secondary text-decoration-none">condizioni dei servizi internet</a></Col>
+                <Col xs={2} className="border-end border-secondary px-1">Apple Music e privacy</Col>
+                <Col xs={2} className="border-end border-secondary px-1"> Avviso sui cookie</Col>
+                <Col xs={2} className="border-end border-secondary px-1">Supporto</Col>
                 <Col xs={2} className=" px-1">Feedback</Col>
             </Row>
         </footer>
